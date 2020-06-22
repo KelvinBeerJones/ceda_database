c9abe142d3fdcf9fff0ca513b9055b2fdeb5cf02
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-06-22 12:07:26 +0100

    finished script to insert/update database with kelvin's data

--- convert_kgb_to_sql/script.py
+++ convert_kgb_to_sql/script.py
@@ -1,9 +1,28 @@
 import csv
 
+# Path of the input and output data files
+# input_data_file = "C:\\Users\\kelvi\\DataShare\\data\\database_record_creation\\kbj_data\\sql_aps_qca_qfhs_data.csv"
+input_data_file = "/Users/mike/DataShare/data (Kelvin Beer-Jones)/database_record_creation/kbj_data/sql_aps_qca_qfhs_data.csv"
+output_data_file = "/Users/mike/DataShare/ceda-database (Kelvin Beer-Jones)/convert_kgb_to_sql/output.sql"
+
+# Built string of SQL throughout script to write to file at end of script
 sql_output = ""
 
+# Values from database, required when inserting new data in script
+db_person_id = 2260 # the current max id value of person in db, used to set id of new person values
+db_data_source_ids = {
+    'RAI' : 1,
+    'QCA' : 2,
+    'APS' : 3
+}
+
+# function to replace empty strings from csv with 'NULL' 
+def value_or_null(value, valueisastring=False):
+    v = value if valueisastring != True else '"{}"'.format(value.replace('"', "'"))
+    return v if value != '' else "NULL"
+
 # open file
-with open("C:\\Users\\kelvi\\DataShare\\data\\database_record_creation\\kbj_data\\sql_aps_qca_qfhs_data.csv", newline='') as csvfile:
+with open(input_data_file, newline='') as csvfile:
 
     # get data from file
     data = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -16,38 +35,56 @@ with open("C:\\Users\\kelvi\\DataShare\\data\\database_record_creation\\kbj_data
 
         # create variables for each field in record
         # db table: person
-        row_id = row[0]
-        row_data_source_id = row[1]
-        row_family_name = row[2]
-        row_first_names = row[3]
-        row_title = row[4]
+        row_id = value_or_null(row[0])
+        row_data_source_id = value_or_null(row[1])
+        row_family_name = value_or_null(row[2], True)
+        row_first_names = value_or_null(row[3], True)
+        row_title = value_or_null(row[4], True)
         # db table: m2m_person_ceda
-        row_aps_first_year = row[5]
-        row_aps_last_year = row[6]
-        row_cqa_first_year = row[7]
-        row_cqa_last_year = row[8]
+        row_aps_first_year = value_or_null(row[5])
+        row_aps_last_year = value_or_null(row[6])
+        row_qca_first_year = value_or_null(row[7])
+        row_qca_last_year = value_or_null(row[8])
         # db table: m2m_person_religion
-        row_religion = row[9]
-        row_confirmed = row[10]
-        row_notes = row[11]
-
-
-
+        row_religion = 1 if row[9] == "Quaker" else "NULL" # set to id of 'Quaker' in religion table (1) or NULL 
+        row_confirmed = 1 if row[10] == '1' else 0 # if anything but 1 (true) set to 0 (false) - e.g. if blank
+        row_notes = value_or_null(row[11], True)
 
         # only insert new data in person table if no Id is provided (as these are new people)
-        if row_id == '':
+        if row_id == 'NULL':
 
             # person table
+            db_person_id += 1
             sql_output += """
-INSERT INTO person(family_name, first_names, title_id, data_source_id)
-VALUES ("{}", "{}", {}, {})
-""".format(row_family_name, row_first_names, row_title, row_data_source_id)
+INSERT INTO person(id, family_name, first_names, title, data_source_id)
+VALUES ({}, {}, {}, {}, {});
+""".format(db_person_id, row_family_name, row_first_names, row_title, db_data_source_ids[row_data_source_id])
 
-        # for all records, insert into m2m tables
+            person_fk_id = db_person_id
+        
+        # else, if the db_person_id is known (is an existing person) set it as the person_fk_id to be used in m2m inserts below
+        else:
+            person_fk_id = row_id
 
-        # m2m_person_ceda table
+        # m2m_person_ceda table for APS (if first or last year provided)
+        if row_aps_first_year != 'NULL' or row_aps_last_year != 'NULL':
+            sql_output += """
+INSERT INTO m2m_person_ceda(person_id, ceda_id, first_year, last_year)
+VALUES ({}, {}, {}, {});
+""".format(person_fk_id, 2, row_aps_first_year, row_aps_last_year)
+
+        # m2m_person_ceda table for QCA (if first or last year provided)
+        if row_qca_first_year != 'NULL' or row_qca_last_year != 'NULL':
+            sql_output += """
+INSERT INTO m2m_person_ceda(person_id, ceda_id, first_year, last_year)
+VALUES ({}, {}, {}, {});
+""".format(person_fk_id, 1, row_qca_first_year, row_qca_last_year)
+
+        # m2m_person_religion table
         sql_output += """
-INSERT INTO m2m_person_ceda
-"""
+INSERT INTO m2m_person_religion(person_id, religion_id, confirmed, notes)
+VALUES ({}, {}, {}, {});
+""".format(person_fk_id, row_religion, row_confirmed, row_notes)
 
-print(sql_output)
+with open(output_data_file, "w") as file:
+    file.write(sql_output)
