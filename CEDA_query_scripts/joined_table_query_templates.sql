6b53b7a5f266e1c67a5f8580d260a52c0632ac92
Kelvin <kgb650@student.bham.ac.uk>
2020-06-30 12:21:33 +0100

    Mods now that CEDA located in own table

--- CEDA_query_scripts/joined_table_query_templates.sql
+++ CEDA_query_scripts/joined_table_query_templates.sql
@@ -4,9 +4,23 @@
 
 -- The templates below address all tables linked to the person table
 
+-- list all people in one club
+SELECT * FROM m2m_person_club WHERE club_id = 13;
 
+--count members of every club	
+	
+SELECT m2m_person_club.club_id,
+		club.name AS club_name,
+		COUNT(m2m_person_club.person_id) AS total_members
+-- Second locate the tables where each of the chosen columns can be found in FROM
+FROM person, club, m2m_person_club
+-- Join the tables together by linking the primary keys to theri respective foreign keys
+WHERE person.id = m2m_person_club.person_id 
+		AND club.id = m2m_person_club.club_id
+GROUP BY m2m_person_club.club_id
+ORDER BY total_members DESC;
 
---CLUB = Athaneum
+--list all members of one club, CLUB = Athaneum
 -- first choose the columns to show in output table by SELECT
 SELECT person.id,
 	   person.family_name,
@@ -20,8 +34,7 @@ WHERE person.id = m2m_person_club.person_id
 -- Filter the search to output the chosen columns.
 		AND club.id = 2;
 	
-	
---count all memebrs of an entity	
+--count all memebrs of aAthanaeum club	
 	
 SELECT COUNT(*) AS total_members_athaneum
 -- Second locate the tables where each of the chosen columns can be found in FROM
@@ -33,21 +46,7 @@ WHERE person.id = m2m_person_club.person_id
 		AND club.id = 2;
 
 	
---Find how many members of all clubs	
-	
-SELECT m2m_person_club.club_id,
-		club.name AS club_name,
-		COUNT(m2m_person_club.person_id) AS total_members
--- Second locate the tables where each of the chosen columns can be found in FROM
-FROM person, club, m2m_person_club
--- Join the tables together by linking the primary keys to theri respective foreign keys
-WHERE person.id = m2m_person_club.person_id 
-		AND club.id = m2m_person_club.club_id
-GROUP BY m2m_person_club.club_id
-ORDER BY total_members DESC;
 
--- Check all people in a club
-SELECT * FROM m2m_person_club WHERE club_id = 13;		
 	
 	
 	
@@ -65,9 +64,7 @@ WHERE person.id = m2m_person_location.person_id
 -- Filter the search to output the chosen columns.
 		AND location.id = 1;	
 	
-	
-	
-	
+		
 --OCCUPATION = Academic	
 -- first choose the columns to show in output table by SELECT
 SELECT person.id,
@@ -83,8 +80,6 @@ WHERE person.id = m2m_person_occupation.person_id
 		AND occupation.id = 7;		
 
 	
-	
-	
 --RELIGION = Quaker	* NOTE: There is no data yet for this query!
 -- first choose the columns to show in output table by SELECT
 SELECT person.id,
@@ -99,9 +94,7 @@ WHERE person.id = m2m_person_religion.person_id
 -- Filter the search to output the chosen columns.
 		AND religion.id = 1;		
 		
-	
-	
-	
+
 	
 --SOCIETY = Royal College of Surgeons
 -- first choose the columns to show in output table by SELECT
@@ -117,9 +110,7 @@ WHERE person.id = m2m_person_society.person_id
 -- Filter the search to output the chosen columns.
 		AND society.id = 2;		
 		
-	
-	
-	
+		
 --SUFFIX = Fellow of the Royal College of Surgeons
 -- first choose the columns to show in output table by SELECT
 SELECT person.id,
@@ -133,3 +124,17 @@ WHERE person.id = m2m_person_suffix.person_id
 		AND suffix.id = m2m_person_suffix.suffix_id
 -- Filter the search to output the chosen columns.
 		AND suffix.id = 4;		
+		
+
+
+
+
+
+-- find persons not in a ceda. This is important becuase the database is not for non-members!
+
+	
+SELECT id, family_name, first_names 
+FROM person
+WHERE person.id NOT IN
+    (SELECT m2m_person_ceda.person_id 
+     FROM m2m_person_ceda);
